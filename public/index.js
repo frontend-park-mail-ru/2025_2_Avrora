301a0b1b2183cb924e193889f102165dbaa48db0
Vladislav Lokhanev <88274622+VladislavLF@users.noreply.github.com>
2025-09-25 11:07:03 +0300

    Delete public/index.js

--- public/index.js
+++
@@ -1,9 +0,0 @@
-console.log('hello world');
-var btn = document.getElementById('bestButtonEver');
-
-btn.addEventListener('click', ()=> {
-    const elements = document.getElementsByTagName('h1');
-    Array.from(elements).forEach(element => {
-        element.style = 'color: yellow';
-    });
-});
