c8f0e684cd6fad6a96b7c9c493bdbd1b3492ea24
Vladislav Lokhanev <88274622+VladislavLF@users.noreply.github.com>
2025-11-04 20:55:29 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,25 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        }
       },
     },
+    hmr: {
+      overlay: false
+    }
   },
   build: {
     outDir: '../dist',
@@ -22,6 +37,10 @@ export default defineConfig({
     }
   },
   optimizeDeps: {
-    include: ['handlebars']
+    include: ['handlebars'],
+    exclude: ['@vite/client', '@vite/env']
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
   }
 });
